bdcb814bb59be95a9b7fab1045f797bf492900aa
aktsbot <box.ashishk@gmail.com>
2024-02-18 13:00:23 +0530

    remove stuff we dont need

--- app/config.js
+++ app/config.js
@@ -11,12 +11,6 @@ const config = {
   session_secret: process.env.SESSION_SECRET,
   session_cookie_name: `${appName.replace(/\s+/g, "_").toLowerCase()}.sid`, /// The App ==> the_app
 
-  // jwt
-  accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
-  accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY,
-  refreshTokenPrivateKey: process.env.REFRESH_TOKEN_PRIVATE_KEY,
-  refreshTokenPublicKey: process.env.REFRESH_TOKEN_PUBLIC_KEY,
-
   // email
   smtpHost: process.env.SMTP_HOST,
   smtpPort: process.env.SMTP_PORT,
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 
 import authRouter from "./auth.route.js";
-import userRouter from "./user.route.js";
 import generalRouter from "./general.route.js";
 
 const appRouter = Router();
 
 appRouter.use("/auth", authRouter);
-appRouter.use("/users", userRouter);
 
 // should be the last route
 appRouter.use("/", generalRouter);
